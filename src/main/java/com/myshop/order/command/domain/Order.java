6a6be0e5b1150f025c6390a78b1dafe3e1771670
aoqlsdl <bini92444@gmail.com>
2023-03-11 00:07:00 +0900

    [p.32-33] Order, OrderState 로직 구현

--- src/main/java/com/myshop/order/command/domain/Order.java
+++ src/main/java/com/myshop/order/command/domain/Order.java
@@ -11,5 +11,11 @@ public class Order {
 
         this.shippingInfo = newShippingInfo;
     }
-    
+
+//    Order에서 배송지 정보 가능 여부에 대한 로직을 구현해두어야 주문 상태와 다른 정보를 함께 사용할 경우를 대비할 수 있음
+    private boolean isShippingInfoChangeable() {
+        return state == OrderState.PAYMENT_WAITING ||
+                state == OrderState.PREPARING;
+    }
+
 }
